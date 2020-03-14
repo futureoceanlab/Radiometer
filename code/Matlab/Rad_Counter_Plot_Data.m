7682fd91bbe6947b2f3fe0c2b694e36a18d53166
Future Ocean Lab <futureoceanlab@gmail.com>
2020-03-14 17:50:27 -0400

    improved token rejection in matlab script

--- code/Matlab/Rad_Counter_Plot_Data.m
+++ code/Matlab/Rad_Counter_Plot_Data.m
@@ -20,20 +20,25 @@ nPackets    = floor(fileInfo.bytes / 8);
 nHeartBeats = floor(nPackets/(Nsamples+2));
 nMinutes    = floor(nSecs/60);
 
+% Raw data: 
+%    Data per Ping 
+Ping_Data        = zeros(nHeartBeats,4,NSamples);
+%    Data per Heartbeat 
 HeartBeat_UTC_S  = zeros(1,nHeartBeats);
-HeartBeat_UTC_uS = zeros(1,nHeartBeats);
+HeartBeat_UTC_u  = zeros(1,nHeartBeats);
 HeartBeat_X_inc  = zeros(1,nHeartBeats);
 HeartBeat_Y_inc  = zeros(1,nHeartBeats);
 
-Ping_Data        = zeros(nHeartBeats,4,NSamples);
 
-HeartBeat_uSecs  = zeros(1,nHeartBeats);
-HeartBeat_Pulses = zeros(1,nHeartBeats);
-HeartBeat_TimeHi = zeros(1,nHeartBeats);
-
-Minute_uSecs  = zeros(1,nMinutes);
-Minute_Pulses = zeros(1,nMinutes);
-Minute_TimeHi = zeros(1,nMinutes);
+% Derived Data:
+%    Ping Data per Second technically, per HB)
+Data_uSecs_perS  = zeros(1,nHeartBeats);
+Data_Pulses_perS = zeros(1,nHeartBeats);
+Data_TimeHi_perS = zeros(1,nHeartBeats);
+%    Ping Data per Minute technically, per 60 HB)
+Data_uSecs_perM  = zeros(1,nMinutes);
+Data_Pulses_perM = zeros(1,nMinutes);
+Data_TimeHi_perM = zeros(1,nMinutes);
 
 
 for i = 1:nHeartBeats
@@ -41,15 +46,17 @@ Ping_Data(i,:,:)    = fread(hDataFile,[4,Nsamples],'uint16=>uint16');
 
 TokenD              = sum(squeeze(Ping_Data(i,1,:)));
 if (TokenD ~= 64764 * NSamples) %0xFCFC
+    fprintf('Found a wrong Token in Ping_Data! \nShould be [%u], but we got [%u].\n',  64764 * NSamples,TokenA)
     break;
 end
 
-HeartBeat_uSecs(i)  = sum(squeeze(Ping_Data(i,2,:)));
-HeartBeat_Pulses(i) = sum(squeeze(Ping_Data(i,3,:)));
-HeartBeat_TimeHi(i) = sum(squeeze(Ping_Data(i,4,:)));
+Data_uSecs_perS(i)  = sum(squeeze(Ping_Data(i,2,:)));
+Data_Pulses_perS(i) = sum(squeeze(Ping_Data(i,3,:)));
+Data_TimeHi_perS(i) = sum(squeeze(Ping_Data(i,4,:)));
 
 TokenA              = fread(hDataFile,1,'uint16=>uint16');
 if (tokenA ~= 65021) %0xFDFD 
+    fprintf('Found a wrong Token in HeartBeat B! \nShould be [%u], but we got [%u].\n',65021 * NSamples,TokenA)
     break;
 end
 
@@ -58,11 +65,12 @@ HeartBeat_UTC_S(i)      = fread(hDataFile,1,'uint32=>uint32');
 
 TokenB              = fread(hDataFile,1,'uint16=>uint16');
 if (tokenB ~= 65278) %0xFEFE
+    fprintf('Found a wrong Token in HeartBeat B! \nShould be [%u], but we got [%u].\n',65278 * NSamples,TokenB)
     break;
 end
 
 HeartBeat_Y_inc(i)  = fread(hDataFile,1,'uint16=>uint16');
-HeartBeat_UTC_uS(i)     = fread(hDataFile,1,'uint32=>uint32');
+HeartBeat_UTC_u(i)     = fread(hDataFile,1,'uint32=>uint32');
 
 end
 
@@ -72,9 +80,9 @@ for   m  = 1:nMinutes
     
     for  s=1:60
         i = 60*(m-1)+s;
-        Minute_uSecs(m)  = Minute_uSecs(m)  + HeartBeat_uSecs(i);
-        Minute_Pulses(m) = Minute_Pulses(m) + HeartBeat_Pulses(i);
-        Minute_TimeHi(m) = Minute_TimeHi(m) + HeartBeat_TimeHi(i);
+        Data_uSecs_perM(m)  = Data_uSecs_perM(m)  + Data_uSecs_perS(i);
+        Data_Pulses_perM(m) = Data_Pulses_perM(m) + Data_Pulses_perS(i);
+        Data_TimeHi_perM(m) = Data_TimeHi_perM(m) + Data_TimeHi_perS(i);
     end
     
 end
@@ -83,7 +91,7 @@ figure(2)
 clf
 hold on;
 plot(1:nMinutes,log10(Minute_Pulses(1:end)/60),'b','LineWidth',3)
-plot(((1:nHeartBeats)/60),log10(HeartBeat_Pulses(1:nHeartBeats)),'g','LineWidth',1)
+plot(((1:nHeartBeats)/60),log10(Data_Pulses_perS(1:nHeartBeats)),'g','LineWidth',1)
 %plot((61:nMins*60)/60,log10(sCounts(61:nMins*60)),'.','MarkerSize',1)
 %plot((61:nMinutes*60)/60,log10(Minute_Pulses(61:nMins*60)),'g','LineWidth',1)
 hold off;
